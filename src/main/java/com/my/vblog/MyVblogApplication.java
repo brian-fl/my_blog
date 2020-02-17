7ccdae95c9b080bf6f158709a79bffc5492b68ac
Jun.Cheng <jun.cheng@capitalcrux.com>
2020-02-18 07:54:13 +0800

    first commit

---
+++ src/main/java/com/my/vblog/MyVblogApplication.java
@@ -0,0 +1,13 @@
+package com.my.vblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyVblogApplication.class, args);
+    }
+
+}
